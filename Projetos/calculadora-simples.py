0efd26c1b7b893cc577bde8bfa8fa8ba38060643
Bruno AC <brunoacano@gmail.com>
2025-05-28 15:45:30 -0300

    Update calculadora-simples.py
    foi colocado um try e definido uma mensagem caso aconteça um zero division error entre b//a ou a//b

--- Projetos/calculadora-simples.py
+++ Projetos/calculadora-simples.py
@@ -21,35 +21,36 @@ def calculadora(a, b, op):
     elif op == '*':
         return a * b
     elif op == '/':
-        d= input('Deseja calcular o primeiro valor dividido pelo segundo? (s/n) Caso não será calculado o inverso: ').strip().lower()
-        if b == 0 and a == 0:
-            return "Indefinido! Divisão por 0."
-        elif d == 's' and b!=0 and a == 0:
-            return 0
-        elif d == 'n' and a != 0 and b ==0:
-            return 0
-        else:
-            while True:
-                if d == 's':
-                    resto = input('Deseja saber o resto da divisão também? (s/n): ').strip().lower()
-                    while True:
-                        if resto == 's':
-                            return [a // b, a % b]
-                        elif resto == 'n':
-                            return a // b
+            try:
+                if a == 0 and b == 0:
+                    return "Indefinido! Divisão de 0 por 0."
+                elif a//b == 0:
+                    return 0
+                elif a//b != 0 and b//a != 0:
+                    while True: 
+                        d= input('Deseja calcular o primeiro valor dividido pelo segundo? (s/n) Caso não será calculado o inverso: ').strip().lower()
+                        if d == 's':
+                            resto = input('Deseja saber o resto da divisão também? (s/n): ').strip().lower()
+                            while True:
+                                    if resto == 's':
+                                        return [a // b, a % b]
+                                    elif resto == 'n':
+                                        return a // b
+                                    else:
+                                        print("Opção inválida. Por favor, digite 's' ou 'n'.")
+                        elif d == 'n':
+                            resto = input('Deseja saber o resto da divisão também? (s/n): ').strip().lower()
+                            while True:
+                                if resto == 's':
+                                    return [b // a, b % a]
+                                elif resto == 'n':
+                                    return b // a
+                                else:
+                                    print("Opção inválida. Por favor, digite 's' ou 'n'.")
                         else:
                             print("Opção inválida. Por favor, digite 's' ou 'n'.")
-                elif d == 'n':
-                    resto = input('Deseja saber o resto da divisão também? (s/n): ').strip().lower()
-                    while True:
-                        if resto == 's':
-                            return [b // a, b % a]
-                        elif resto == 'n':
-                            return b // a
-                        else:
-                            print("Opção inválida. Por favor, digite 's' ou 'n'.")
-            else:
-                print("Opção inválida. Por favor, digite 's' ou 'n'.")
+            except ZeroDivisionError:
+                return 0      
     else:
         return "Operação inválida. Por favor, escolha uma operação válida."
     
