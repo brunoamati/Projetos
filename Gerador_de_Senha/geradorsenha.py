5cf4e002e4ac61be106ef517d5428b73e35a2219
Bruno AC <brunoacano@gmail.com>
2025-09-26 14:55:54 -0300

    Update geradorsenha.py

--- Gerador_de_Senha/geradorsenha.py
+++ Gerador_de_Senha/geradorsenha.py
@@ -2,8 +2,12 @@ import random
 import string
 
 def gerador(j):
-    
+    modulosenha = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+    random.choice(modulosenha)
 
 while True:
     print("Bem vindo ao gerador de senha:".center(30,'-'))
+    print(gerador(10))
+    
     
