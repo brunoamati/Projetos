a37561d9dc6900b7e92e4b0f190d2234f93414c9
Bruno AC <brunoacano@gmail.com>
2026-02-18 20:27:07 -0300

    adicionada uma função de sim e não
    colocada uma função de sim e não

--- Gerador_de_Senha/geradorsenha.py
+++ Gerador_de_Senha/geradorsenha.py
@@ -13,30 +13,29 @@ def tamanho(tam):
             print('ERRO: Digite apenas numeros inteiros!')
 
 def gerador(j):
-    h = ''
+    senha = ''
+
     modulosenha = string.ascii_letters + string.digits + string.punctuation
 
     for i in range(j):
-       h += random.choice(modulosenha)
-    return h
+       senha += random.choice(modulosenha)
+    return senha
 
+def s_n(msg):
+   while True:
+        resp = input(msg).strip().lower()
+        if resp == 's': return True
+        if resp == 'n': return False
+        print("Responda apenas com 's' ou 'n'.")
+ 
 loop = 1
 while loop == 1:
     print("Bem vindo ao gerador de senha!".center(40, '-'))
     tam = tamanho("Digite o tamanho da senha: ")
     senha = gerador(tam)
     print(f"Senha gerada: {senha}")
-    while True:
-        cont = input('Deseja gerar outra senha?(s/n): ').strip().lower()
-        if cont == 'n':
-            loop = 0
-            print("Obrigado por usar meu gerador!")
-            break
-        elif cont == 's':
-            loop = 1
-            print(f'Retornando ao inicio!')
-            break
-        else:
-            print("Opção inválida. Por favor, digite 's' ou 'n'.")
+    if not s_n("Deseja gerar outra senha? (s/n): "):
+        print("Até a próxima!")
+        break
     
     
