2f2db57d8e93c748700b52e072073f2006ac456d
Bruno AC <brunoacano@gmail.com>
2026-02-18 21:16:58 -0300

    Função de complexidade
    Colcoada uma função para ver sobre a complexidade da senha

--- Gerador_de_Senha/geradorsenha.py
+++ Gerador_de_Senha/geradorsenha.py
@@ -30,18 +30,36 @@ def s_n(msg):
         if resp == 's': return True
         if resp == 'n': return False
         print("Responda apenas com 's' ou 'n'.")
- 
-loop = 1
-while loop == 1:
+
+def complex(len, let, num, simb):
+    prefs = "" 
+    if let: prefs += string.ascii_letters
+    if num: prefs += string.digits
+    if simb: prefs += string.punctuation
+
+    pontos = 0
+    if tam > 12: pontos +=1
+    if string.ascii_letters + string.digits in prefs: pontos += 1
+    if string.punctuation in prefs: pontos += 1
+    if pontos == 3:
+        return 'Essa é uma senha forte!'
+    elif pontos == 2:
+        return 'Essa é uma senha mediana!'
+    else:
+        return 'Essa é uma senha fraca!'
+
+while True:
     print("Bem vindo ao gerador de senha!".center(40, '-'))
     tam = tamanho("Digite o tamanho da senha: ")
     letras = s_n("Deseja incluir letras?(s/n): ")
     numeros = s_n("Deseja incluir numeros?(s/n): ")
     simbolos = s_n("Deseja incluir simbolos?(s/n): ")
     senha = gerador(tam,letras,numeros,simbolos)
+    complexidade = complex(tam,letras,numeros,simbolos)
     
     if senha:
         print(f"Senha gerada: {senha}")
+        print(complexidade)
     else:
         print('\nERRO: Nenhuma senha gerada, precisa escolher ao menos 1 tipo de caractere!')
     
