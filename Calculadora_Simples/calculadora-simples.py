a8596d4f3bc36b30742bf6f5735a1f77aa504c2c
Bruno AC <brunoacano@gmail.com>
2026-02-19 06:50:36 -0300

    Limpeza no codigo
    Foi realizada uma limpeza de repetições no codigo, alem de uma adição para realizar a limpeza do console para cada vez que o usuario for realizar uma operação

--- Calculadora_Simples/calculadora-simples.py
+++ Calculadora_Simples/calculadora-simples.py
@@ -1,3 +1,6 @@
+import os
+
+
 def pegarNumero(msg):
     while True:
         try:
@@ -6,47 +9,37 @@ def pegarNumero(msg):
         except ValueError:
             print("Entrada inválida. Por favor, digite um número válido.")
 
+
 def calculadora(a, b, op):
-    
+
     if op == '+':
         return f'Soma dos valores: {a + b}'
     elif op == '-':
-            d = s_n('Deseja calcular o primeiro valor menos o segundo? (s/n) Caso não será calculado o inverso: ')
-            if d == True:
-                return f'Subtração dos valores: {a - b}'
-            else:
-                return f'Subtração dos valores invertidos: {b - a}'
+        d = s_n('Deseja calcular o primeiro valor menos o segundo? (s/n) Caso não será calculado o inverso: ')
+        if not d:
+            a, b = b, a
+        return f'Subtração dos valores: {a - b}'
     elif op == '*':
         return f'Multiplicação dos valores: {a * b}'
     elif op == '/':
-            try:
-                if a == 0 and b == 0:
-                    return "Indefinido! Divisão de 0 por 0."
-                else:
-                    d= s_n('Deseja calcular o primeiro valor dividido pelo segundo? (s/n) Caso não será calculado o inverso: ')
-                    if d == True:
-                            resto = s_n('Deseja saber o resto da divisão?(s/n):')
-                            if resto == True:
-                                return (f'Divisão inteira: {a // b}\nDivisão real: {round(a/b, 2)}\nResto: {a % b}')
-                            else:
-                                return (f'Divisão inteira: {a // b}\nDivisão real: {round(a/b, 2)}')
-                    else:
-                        resto = s_n('Deseja saber o resto da divisão também? (s/n): ')
-                        if resto == True:
-                            return (f'Divisão inteira: {b // a}\nDivisão real: {round(b/a, 2)}\nResto: {b % a}')
-                        else:
-                            return (f'Divisão inteira: {b // a}\nDivisão real: {round(b/a, 2)}')
-            except ZeroDivisionError:
-                return ('ERRO! Divisão com 0, impossivel determinar o valor')  
-    elif op == '**':
-            d = s_n('Deseja calcular o primeiro valor potencializado pelo o segundo? (s/n) Caso não será calculado o inverso: ')
-            if d == True:
-                return f'Potencialização dos valores: {a ** b}'
+        try:
+            if a == 0 and b == 0:
+                return "Indefinido! Divisão de 0 por 0."
             else:
-                return f'Potencialização dos valores invertidos: {b ** a}'
-
+                d = s_n('Deseja calcular o primeiro valor dividido pelo segundo? (s/n) Caso não será calculado o inverso: ')
+                if not d:
+                    a, b = b, a
+                return f'Divisão inteira: {a // b}\nDivisão real: {round(a/b, 2)}\nResto: {a % b}'
+        except ZeroDivisionError:
+            return 'ERRO! Divisão com 0, impossivel determinar o valor'
+    elif op == '**':
+        d = s_n('Deseja calcular o primeiro valor potencializado pelo o segundo? (s/n) Caso não será calculado o inverso: ')
+        if not d:
+            a, b = b, a
+        return f'Potencialização dos valores: {a ** b}'
     else:
         return "Operação inválida. Por favor, escolha uma operação válida."
+
 
 def s_n(msg):
     while True:
@@ -58,12 +51,13 @@ def s_n(msg):
         print("Responda apenas com 's' ou 'n'.")
 
 
-print("Calculadora Simples".center(30, '-'))
-print("Somente serão realizados cálculos com números inteiros.")  
+
 while True:
     print("Escolha uma operação para realizar:\n1. Adição (+)\n2. Subtração (-)\n3. Multiplicação (*)\n4. Divisão (/)\n5. Potencialização(**)")
 
     while True:
+        print("Calculadora Simples".center(30, '-'))
+        print("Somente serão realizados cálculos com números inteiros.")
         op = input("Escolha uma operação (digite o símbolo): ").strip()
         if op in ['+', '-', '*', '/', '**']:
             a = pegarNumero("Digite o primeiro número: ")
@@ -77,3 +71,5 @@ while True:
     if not s_n('\nDeseja realizar outra operação?(s/n):'):
         print('Até mais! Obrigado por usar minha calculadora!')
         break
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
